8dc7947384a988b3596be4e6b9891df3f024bc53
Rimaro <leonardo.ongaro2003@hotmail.com>
2022-04-07 14:32:13 +0200

    nothing rilevant

--- src/components/subpages/Assenze/Assenze.js
+++ src/components/subpages/Assenze/Assenze.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useCookies } from "react-cookie";
 import getAssenze from "../../../api/getAssenze";
 import Header from "../../Header/Header";
@@ -8,7 +8,7 @@ import { Toolbar } from "@mui/material";
 import { Typography } from "@mui/material";
 import { red, green } from '@mui/material/colors';
 
-export default function Assenze () {
+export default function Assenze() {
     const [cookies, setCookies] = useCookies();
     const [assenze, setAssenze] = useState([]);
 
@@ -23,70 +23,57 @@ export default function Assenze () {
             })
     }, [])
 
-    return(
+    const drawerWidth = 300;
+
+    return (
         <Box sx={{ display: 'flex' }}>
-        <Header />
-        <Menu />
-        <Box
-            component="main"
-            sx={{ flexGrow: 1, bgcolor: 'background.default', p: 3, width: `calc(100% - ${drawerWidth}px)`, ml: `${drawerWidth / 5}px` }}
-        >
-            <Toolbar />
-            <Typography
-                variant="h4"
+            <Header />
+            <Menu />
+            <Box
+                component="main"
+                sx={{ flexGrow: 1, bgcolor: 'background.default', p: 3, width: `calc(100% - ${drawerWidth}px)`, ml: `${drawerWidth / 5}px` }}
             >
-                Voti
-            </Typography>
-            <List>
-                {voti.map((item, index) => {
-                    let voto = item.codVoto;
-                    if (voto.includes("10")) {
-                        voto = Number(item.codVoto.slice(0, 2))
-                    }
-                    else {
-                        voto = Number(item.codVoto.slice(0, 1))
-                    }
+                <Toolbar />
+                <Typography
+                    variant="h4"
+                >
+                    Assenze
+                </Typography>
+                <List>
+                    {assenze.map((item, index) => {
+                        return (
+                            <ListItem key={index} sx={{ border: "1px solid #ccc", borderRadius: "5px", margin: 1 }}>
+                                <ListItemText
+                                    primary={
+                                        <React.Fragment>
+                                            <Typography
+                                                sx={{ display: 'inline', fontWeight: 600 }}
+                                                component="span"
+                                            >
+                                                Assenza del {item.datAssenza}
+                                            </Typography>
+                                        </React.Fragment>
+                                    }
+                                    secondary={
+                                        <React.Fragment>
+                                            {item.registrataDa}
+                                            <br />
+                                            <Typography
+                                                sx={{ display: 'inline' }}
+                                                component="span"
+                                                variant="body2"
+                                                color="text.primary"
 
-                    let avatarColor = "";
-                    voto < 6 ? avatarColor = red[500] : avatarColor = green[500];
-
-                    return (
-                        <ListItem key={index} sx={{ border: "1px solid #ccc", borderRadius: "5px", margin: 1 }}>
-                            <ListItemAvatar>
-                                <Avatar sx={{ bgcolor: avatarColor }}>{voto}</Avatar>
-                            </ListItemAvatar>
-                            <ListItemText
-                                primary={
-                                    <React.Fragment>
-                                        <Typography
-                                            sx={{ display: 'inline', fontWeight: 600 }}
-                                            component="span"
-                                        >
-                                            {item.desMateria}
-                                        </Typography>
-                                    </React.Fragment>
-                                }
-                                secondary={
-                                    <React.Fragment>
-                                        <Typography
-                                            sx={{ display: 'inline' }}
-                                            component="span"
-                                            variant="body2"
-                                            color="text.primary"
-
-                                        >
-                                            {item.desProva}
-                                        </Typography>
-                                        <br />
-                                        {item.docente}
-                                    </React.Fragment>
-                                }
-                            />
-                        </ListItem>
-                    );
-                })}
-            </List>
+                                            >
+                                            </Typography>
+                                        </React.Fragment>
+                                    }
+                                />
+                            </ListItem>
+                        );
+                    })}
+                </List>
+            </Box>
         </Box>
-    </Box>
     );
 }

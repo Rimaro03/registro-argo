8284b3390e7e883590889ae5482834f495dcac9d
Rimaro <leonardo.ongaro2003@hotmail.com>
2022-04-14 21:24:11 +0200

    added dark mode, TODO: 6light dark chooser

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import Login from './components/Login/Login';
 import Riepilogo from './components/subpages/Riepilogo/Riepilogo';
 import Voti from './components/subpages/Voti/Voti';
@@ -9,25 +10,36 @@ import Argomenti from './components/subpages/Argomenti/Argomenti';
 import Promemoria from './components/subpages/Promemoria/Promemoria';
 import NoMatch from './components/NoMatch/NoMatch';
 import { Routes, Route } from "react-router-dom";
-
+import { useTheme, ThemeProvider, createTheme } from '@mui/material/styles';
 
 function App() {
+  const mode = 'dark';
+  const theme = React.useMemo(
+    () =>
+      createTheme({
+        palette: {
+          mode,
+        },
+      }),
+    [mode],
+  );
+
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <Routes>
-        <Route path="/" element={ <Login /> } />
-        <Route path="/login" element={ <Login /> } />
-        <Route path="/riepilogo" element={ <Riepilogo /> } />
-        <Route path="/voti" element={ <Voti /> } />
-        <Route path="/assenze" element={ <Assenze /> } />
-        <Route path="/note" element={ <Note /> } />
-        <Route path="/scrutinio" element={ <Scrutinio /> } />
-        <Route path="/compiti" element={ <Compiti /> } />
-        <Route path="/argomenti" element={ <Argomenti /> } />
-        <Route path="/promemoria" element={ <Promemoria /> } />
-        <Route path="*" element={ <NoMatch /> }/>
+        <Route path="/" element={<Login />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/riepilogo" element={<Riepilogo />} />
+        <Route path="/voti" element={<Voti />} />
+        <Route path="/assenze" element={<Assenze />} />
+        <Route path="/note" element={<Note />} />
+        <Route path="/scrutinio" element={<Scrutinio />} />
+        <Route path="/compiti" element={<Compiti />} />
+        <Route path="/argomenti" element={<Argomenti />} />
+        <Route path="/promemoria" element={<Promemoria />} />
+        <Route path="*" element={<NoMatch />} />
       </Routes>
-    </>
+    </ThemeProvider>
   );
 }
 
